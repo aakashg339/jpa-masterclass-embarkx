f7079f9e5e4011d2861825807a00d66a97de3362
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-01-13 07:04:24 +0530

    refactor(spring-boot-jpa): Migrated from jpa to spring data jpa

--- spring-boot-jpa/src/main/java/com/example/App.java
+++ spring-boot-jpa/src/main/java/com/example/App.java
@@ -1,31 +1,51 @@
 package com.example;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
 import com.example.model.UserInfo;
-import com.example.util.JPAUtil;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
 public class App 
 {
+
     public static void main( String[] args )
     {
-        EntityManager entityManager = JPAUtil.getEntityManager();
+        SpringApplication.run(App.class, args);
+    }
 
+}
+
+@Component
+class JPAService implements CommandLineRunner {
+    
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    @Transactional
+    public void run(String... args) throws Exception {
         try {
             UserInfo user = new UserInfo("Alice", "Alice@email.com");
 
-            entityManager.getTransaction().begin();
+            // entityManager.getTransaction().begin();
             entityManager.persist(user);
-            entityManager.getTransaction().commit();
+            // entityManager.getTransaction().commit();
 
             System.out.println("User saved with ID: " + user.getId());
         } finally {
-            entityManager.close();
-            JPAUtil.close();
+            // entityManager.close();
         }
     }
+
 }
