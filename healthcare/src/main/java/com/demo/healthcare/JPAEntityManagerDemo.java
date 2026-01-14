3fa1f0cdb1bc1d36bb985953064778b8d3f56d3d
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-01-14 11:01:59 +0530

    feat(healthcare): Added examples with entity manager got managed and detached entity

--- healthcare/src/main/java/com/demo/healthcare/JPAEntityManagerDemo.java
+++ healthcare/src/main/java/com/demo/healthcare/JPAEntityManagerDemo.java
@@ -1,18 +1,37 @@
 package com.demo.healthcare;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.demo.healthcare.model.Patient;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 
+@Component
 public class JPAEntityManagerDemo implements CommandLineRunner {
 
     @PersistenceContext
     private EntityManager entityManager;
 
     @Override
+    @Transactional
     public void run(String... args) throws Exception {
-        
+        Patient patient = new Patient("John Doe", 30);
+        entityManager.persist(patient);
+        // System.out.println("Persisted Patient: " + p);
+
+        // Managed entity
+        // patient.setName("John Doe Updated");
+        // patient.setAge(95);
+        // System.out.println("Persisted Patient: " + patient);
+    
+        // Detached
+        entityManager.detach(patient);
+        // patient.setName("John Doe Updated");
+        // patient.setAge(95);
+
     }
 
 }
