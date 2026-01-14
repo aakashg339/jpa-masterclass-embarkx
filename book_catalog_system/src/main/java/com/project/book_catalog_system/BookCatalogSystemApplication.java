f4ced12eac46e6a3fbfb5c8ad2d66b0783abef67
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-01-14 05:50:06 +0530

    chore(book_catalog_system): created spring boot maven project for book catalog system

---
+++ book_catalog_system/src/main/java/com/project/book_catalog_system/BookCatalogSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.book_catalog_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCatalogSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookCatalogSystemApplication.class, args);
+	}
+
+}
